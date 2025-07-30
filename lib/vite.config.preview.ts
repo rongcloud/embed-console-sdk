d61166c29de4da796dfb5ddb5afd5bc8cf910758
lihongzhao <lihongzhao@rongcloud.cn>
2025-07-30 16:33:07 +0800

    feat: 新增嵌入式组件相关文件，包括 embed.es.js、index.html、样式文件和构建配置，支持动态创建和管理组件实例，完善事件处理机制。

--- lib/vite.config.preview.ts
+++ lib/vite.config.preview.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// @ts-ignore
+const base = process.env.NODE_ENV === 'production' ? 'https://rongcloud.github.io/embed-console-sdk/' : '/'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   build: {
-    outDir: '../example',
+    outDir: '../docs',
   }
 })
